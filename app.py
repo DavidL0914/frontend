c4dd54cb9abdef7d6a173a8c87bfe144c537189c
AustinZhang1 <austinzhang0508@gmail.com>
2023-10-25 23:41:47 -0700

    weatherapi change to temperature every hour

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ import time
 from datetime import date
 from flask import Flask, render_template
 
-response = requests.get("https://api.open-meteo.com/v1/forecast?latitude=32.7157&longitude=-117.1647&current=temperature_2m,relativehumidity_2m,apparent_temperature,is_day,weathercode&temperature_unit=fahrenheit&timezone=America%2FLos_Angeles&forecast_days=1")
+response = requests.get("https://api.open-meteo.com/v1/forecast?latitude=32.7157&longitude=-117.1647&hourly=temperature_2m,relativehumidity_2m,precipitation_probability,weathercode&timezone=America%2FLos_Angeles&forecast_days=1")
 weather = response.json()
 app = Flask(__name__)
 
 @app.route('/')
 
 def index():
-    return render_template('weatherapi.html', info=weather)
+    return render_template('weathertesting.html', info=weather)
 
 if __name__ == '__main__':
     app.run(debug=True)
