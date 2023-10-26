1106038b397249d5da6eaf1caa65048fcd7c371e
root <root@AustinLaptop>
2023-10-26 08:50:22 -0700

    weatherapi additions and img

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import time
 from datetime import date
 from flask import Flask, render_template
 
-response = requests.get("https://api.open-meteo.com/v1/forecast?latitude=32.7157&longitude=-117.1647&hourly=temperature_2m,relativehumidity_2m,precipitation_probability,weathercode&timezone=America%2FLos_Angeles&forecast_days=1")
+response = requests.get("https://api.open-meteo.com/v1/forecast?latitude=32.7157&longitude=-117.1647&hourly=temperature_2m,relativehumidity_2m,precipitation_probability,weathercode&temperature_unit=fahrenheit&timezone=America%2FLos_Angeles&forecast_days=1")
 weather = response.json()
 app = Flask(__name__)
 
